3e7eaeb5108ec5aa154a3dfa6ffafd934a270296
ian <iclark@idevops.io>
2018-04-09 16:13:35 -0600

    Updated names of things. Needs testing

--- sitedjango/Site_Name/Site_Name/wsgi.py
+++ sitedjango/Site_Name/Site_Name/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for Ian project.
+WSGI config for Site_Name project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Ian.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "Site_Name.settings")
 
 application = get_wsgi_application()
